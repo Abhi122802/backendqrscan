b422bb6877b31d2cfeea87b27d85ca247027ab28
abhi <abhibubane12@gmail.com>
2025-11-17 15:14:17 +0530

    vercel backend fix

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,18 +1,19 @@
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import { response } from "../utils/helpers.js";
 
 export const authenticate = async (req, res, next) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
 
     if (!token)
-      return res.status(401).json({ message: "No token" });
+      return response(res, 401, "Authorization token not found.");
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded; // The decoded token now becomes the user object, containing the id
 
     next();
   } catch (e) {
-    res.status(401).json({ message: "Invalid token" });
+    return response(res, 401, "Invalid or expired token.");
   }
 }

31b07921952085f756a8ee433f8b3fbeefcaff79
abhi <abhibubane12@gmail.com>
2025-11-17 14:59:43 +0530

    vercel backend fix

--- controllers/qrcodeController.js
+++ controllers/qrcodeController.js
@@ -1,20 +1,21 @@
 import QRCodeModel from "../models/QRCode.js";
+import { response } from "../utils/helpers.js";
 
 export const verifyQRCode = async (req, res) => {
   try {
     const { code } = req.body;
 
+    if (!code) {
+      return response(res, 400, "QR code is required.");
+    }
+
     const qr = await QRCodeModel.findOne({ code });
 
-    if (!qr)
-      return res.json({ status: "inactive", message: "QR not found" });
+    if (!qr) return response(res, 404, "QR code not found or is invalid.");
 
-    res.json({
-      status: qr.status,
-      message: "QR found",
-      qr
-    });
+    return response(res, 200, "QR code verified.", { status: qr.status });
   } catch (e) {
-    res.status(500).json({ error: e.message });
+    console.error("Verify QR Code Error:", e);
+    return response(res, 500, "Server error during QR code verification.");
   }
 };

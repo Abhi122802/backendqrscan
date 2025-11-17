f83013dc58e52f4f02390cfd54e3a6d564e39c3b
abhi <abhibubane12@gmail.com>
2025-11-17 16:21:05 +0530

    vercel backend fix1

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,30 +3,24 @@ import User from "../models/User.js";
 import { response } from "../utils/helpers.js";
 
 export const protect = async (req, res, next) => {
-  let token;
-
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
     try {
       // Get token from header
-      token = req.headers.authorization.split(" ")[1];
+      const token = req.headers.authorization.split(" ")[1];
 
       // Verify token
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
       // Get user from the token (select '-password' to exclude the password hash)
       req.user = await User.findById(decoded.id).select("-password");
-
-      next();
+      return next();
     } catch (error) {
       console.error("Authentication error:", error);
-      return response(res, 401, "Not authorized, token failed");
+      return response(res, 401, "Not authorized, token failed.");
     }
   }
-
-  if (!token) {
-    return response(res, 401, "Not authorized, no token");
-  }
+  return response(res, 401, "Not authorized, no token provided.");
 };

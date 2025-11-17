5c77bc53c637835713c0cf73da267bf4a59a4cc9
abhi <abhibubane12@gmail.com>
2025-11-17 14:04:44 +0530

    vercel backend fix

---
+++ api/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("../config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("QR Backend is running from Vercel!");
+});
+
+// Routes
+app.use("/api/qrcode", require("../routes/qrcodeRoutes"));
+
+module.exports = app;

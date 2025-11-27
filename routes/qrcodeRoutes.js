54e52a69ddcc8b1a94e7c623938bd7c0f00bae7c
abhi <abhibubane12@gmail.com>
2025-11-27 07:45:19 +0530

    vercel backend fix2

--- routes/qrcodeRoutes.js
+++ routes/qrcodeRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createQRCodes } from "../controllers/qrcodeController.js";
+import {
+  createQRCodes,
+  verifyQRCode,
+} from "../controllers/qrcodeController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/", protect, createQRCodes);
+router.post("/verify", verifyQRCode); // Add this line to activate the route
 
 export default router;
